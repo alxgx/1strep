0c975added3eae99ab7b7119ccaf765e30033558
alxgx <alx.g1024@gmail.com>
2023-09-17 15:23:31 +0300

    add .idea

---
+++ HomeWork3_1.py
@@ -0,0 +1,16 @@
+# заменить пробелы тире 1
+userstring = input('enter 12345 by words')
+print(userstring)
+devidedstr = userstring.split(' ')
+# print(devidedstr)
+print('-'.join(devidedstr))
+
+# заменить пробелы тире 2
+
+
+
+
+
+
+
+
